8653df763e40f89695c1a8c3a22f34626b752649
neox1n-dev <76614550+neox1n-dev@users.noreply.github.com>
2021-03-08 10:43:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ $(document).ready(function(){
         e.preventDefault();
         $.ajax({
             type: "POST",
-            url: "mailer/smart.php",
+            url: "/mailer/smart.php",
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
